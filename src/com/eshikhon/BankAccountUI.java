8e6c47f6cc8324ef34a926189316af1911c37432
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-13 22:28:41 +0600

    runnig new account list/ task for calss 12

--- src/com/eshikhon/BankAccountUI.java
+++ src/com/eshikhon/BankAccountUI.java
@@ -3,8 +3,9 @@ package com.eshikhon;
 import java.util.Scanner;
 
 public class BankAccountUI {
-    private Scanner scanner =  new Scanner(System.in);;
-    private Account account;
+    private Scanner scanner =  new Scanner(System.in);
+    private Account[] accountList = new Account[100];
+    private int accountCount = 0;
 
     public void runBanAccount() {
         do {
@@ -22,7 +23,12 @@ public class BankAccountUI {
                     acccoutCreateFunctionality();
                     break;
                 case 2:
-                    System.out.println(account.getBalance());
+                    System.out.print("Enter your account ID: ");
+                    scanner.nextLine();
+                    String id = scanner.nextLine();
+
+                    Account searchAccount = findAccountById(id);
+                    System.out.println(searchAccount.getBalance());
                     break;
                 case 3:
                     depostitTask();
@@ -31,38 +37,70 @@ public class BankAccountUI {
                     withdrawTask();
                     break;
                 case 5:
-                    System.out.println("Last transaction Type " + account.getLastTransactionType());
-                    System.out.println("Last transaction Amount " + account.getLastTransaction());
+                    System.out.print("Enter your account ID: ");
+                    scanner.next();
+                    String id1 = scanner.nextLine();
+
+                    Account account1 = findAccountById(id1);
+
+                    System.out.println("Last transaction Type " + account1.getLastTransactionType());
+                    System.out.println("Last transaction Amount " + account1.getLastTransaction());
                     break;
                 case 6:
                     return;
             }
 
+            System.out.println("\n\n\n\n\n");
         } while (true);
     }
 
     private void acccoutCreateFunctionality() {
         System.out.print("Enter your account Name: ");
-        scanner.next();
+        scanner.nextLine();
         String name = scanner.nextLine();
+
         System.out.print("Enter your account ID: ");
         String id = scanner.nextLine();
+
         System.out.print("Enter your account Initial Balance: ");
         double initialBalance = scanner.nextDouble();
 
-        account = new Account(name, id, initialBalance);
+        Account account = new Account(name, id, initialBalance);
+        accountList[accountCount++] = account;
     }
 
     private void depostitTask() {
+        System.out.print("Enter your account ID: ");
+        scanner.nextLine();
+        String id = scanner.nextLine();
+
+        Account account = findAccountById(id);
+
         System.out.print("Enter your account Deposit amount: ");
         double amount = scanner.nextDouble();
+
         account.deposit(amount);
     }
 
     private void withdrawTask() {
+        System.out.print("Enter your account ID: ");
+        scanner.nextLine();
+        String id = scanner.nextLine();
+
+        Account account = findAccountById(id);
         System.out.print("Enter your account Withdraw amount: ");
         double amountwd = scanner.nextDouble();
+
         account.withdraw(amountwd);
+    }
+
+    private Account findAccountById(String customerId) {
+        for (int i = 0; i < accountList.length; i++) {
+            if (accountList[i].getCustomerID().equals(customerId)) {
+                return accountList[i];
+            }
+        }
+        return null;
     }
 
 }
