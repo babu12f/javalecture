5b65b79ac631563a90c641fb5bce621d4868e9ad
Shofiullah Babor <babu_12f@yahoo.com>
2021-08-22 23:20:59 +0600

    class 20 complete --> class 18

---
+++ src/com/jdbcclass/Class20.java
@@ -0,0 +1,12 @@
+package com.jdbcclass;
+
+public class Class20 {
+
+    public static void main(String[] args) {
+        String str = "Kamaruzzaman vai";
+
+        String str2  = str.replaceAll("z", "BB");
+
+        System.out.println(str2);
+    }
+}
